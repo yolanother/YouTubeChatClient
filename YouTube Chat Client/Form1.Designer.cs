82e79da446aa143080f3985fac9c954c8f2800a8
Aaron Jackson <yolanother@gmail.com>
2021-07-24 22:23:08 -0700

    Connection and auth works

---
+++ YouTube Chat Client/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace YouTube_Chat_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.txtClientId = new System.Windows.Forms.TextBox();
+            this.txtClientSecret = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtChannel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "YouTube Chat Client";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // txtClientId
+            // 
+            this.txtClientId.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtClientId.Location = new System.Drawing.Point(120, 33);
+            this.txtClientId.Name = "txtClientId";
+            this.txtClientId.Size = new System.Drawing.Size(664, 20);
+            this.txtClientId.TabIndex = 1;
+            // 
+            // txtClientSecret
+            // 
+            this.txtClientSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtClientSecret.Location = new System.Drawing.Point(120, 59);
+            this.txtClientSecret.Name = "txtClientSecret";
+            this.txtClientSecret.PasswordChar = '*';
+            this.txtClientSecret.Size = new System.Drawing.Size(664, 20);
+            this.txtClientSecret.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(713, 418);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(798, 412);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtLog);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(790, 386);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Chat Log";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtChannel);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.txtClientId);
+            this.tabPage2.Controls.Add(this.txtClientSecret);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(790, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Settings";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(790, 390);
+            this.txtLog.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Client Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Client Secret";
+            // 
+            // txtChannel
+            // 
+            this.txtChannel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtChannel.Location = new System.Drawing.Point(120, 85);
+            this.txtChannel.Name = "txtChannel";
+            this.txtChannel.PasswordChar = '*';
+            this.txtChannel.Size = new System.Drawing.Size(664, 20);
+            this.txtChannel.TabIndex = 5;
+            this.txtChannel.TextChanged += new System.EventHandler(this.txtChannel_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Channel Id (Optional)";
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(713, 418);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.TabIndex = 5;
+            this.btnJoin.Text = "Join";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Visible = false;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.TextBox txtClientId;
+        private System.Windows.Forms.TextBox txtClientSecret;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtChannel;
+        private System.Windows.Forms.Button btnJoin;
+    }
+}
+
---
+++ YouTube Chat Client/Form1.cs
@@ -0,0 +1,221 @@
+﻿using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YouTube.Base;
+using YouTube.Base.Clients;
+
+namespace YouTube_Chat_Client
+{
+    public partial class Form1 : Form
+    {
+
+        private static SemaphoreSlim fileLock = new SemaphoreSlim(1);
+        private YouTubeConnection connection;
+        private ChatClient client;
+        private Channel channel;
+        public static readonly List<OAuthClientScopeEnum> scopes = new List<OAuthClientScopeEnum>()
+        {
+            OAuthClientScopeEnum.ChannelMemberships,
+            OAuthClientScopeEnum.ManageAccount,
+            OAuthClientScopeEnum.ManageData,
+            OAuthClientScopeEnum.ManagePartner,
+            OAuthClientScopeEnum.ManagePartnerAudit,
+            OAuthClientScopeEnum.ManageVideos,
+            OAuthClientScopeEnum.ReadOnlyAccount,
+            OAuthClientScopeEnum.ViewAnalytics,
+            OAuthClientScopeEnum.ViewMonetaryAnalytics
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            txtClientId.Text = Properties.Settings.Default.clientId;
+            txtClientSecret.Text = Properties.Settings.Default.clientSecret;
+            txtChannel.Text = Properties.Settings.Default.channel;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void ConnectToChannel()
+        {
+            if (null == connection) return;
+
+            if (string.IsNullOrEmpty(txtChannel.Text))
+            {
+                Task.Run(async () =>
+                {
+                    Channel channel = await connection.Channels.GetMyChannel();
+                    ConnectToChannel(channel);
+                });
+            }
+            else
+            {
+                ConnectToChannel("UCWxlUwW9BgGISaakjGM37aw");
+            }
+        }
+
+        private void ConnectToChannel(string channel)
+        {
+            if (null == connection) return;
+
+            Task.Run(async () =>
+            {
+                Channel c = await connection.Channels.GetChannelByID(channel);
+                ConnectToChannel(c);
+            });
+        }
+
+        private async void ConnectToChannel(Channel channel)
+        {
+            if (connection != null)
+            {
+                //
+
+                //Channel channel = await connection.Channels.GetChannelByID("UCyl1z3jo3XHR1riLFKG5UAg");
+
+
+                if (channel != null)
+                {
+                    this.channel = channel;
+                    Log("Connection successful. Joined channel: " + channel.Snippet.Title);
+
+                    //LiveBroadcast broadcast = await connection.LiveBroadcasts.GetChannelActiveBroadcast(channel);
+                    //if (broadcast == null)
+                    //{
+                    //    broadcast = await connection.LiveBroadcasts.GetBroadcastByID("9rCRhTrEpDE");
+                    //    if (broadcast == null)
+                    //    {
+                    //        broadcast = new LiveBroadcast() { Snippet = new LiveBroadcastSnippet() { LiveChatId = "Cg0KC1VxWFFjZmZvTXhjKicKGFVDSHN4NEhxYS0xT1JqUVRoOVRZRGh3dxILVXFYUWNmZm9NeGM" } };
+                    //    }
+                    //}
+
+                    client = new ChatClient(connection);
+                    client.OnMessagesReceived += Client_OnMessagesReceived;
+                    if (await client.Connect())
+                    {
+                        Log("Live chat connection successful!");
+                    }
+                    else
+                    {
+                        Log("Failed to connect to live chat");
+                    }
+
+                    var broadcast = await connection.LiveBroadcasts.GetChannelActiveBroadcast(channel);
+                    if (null != broadcast)
+                    {
+                        var messages = await connection.LiveChat.GetMessages(broadcast);
+                        foreach(var message in messages.Messages)
+                        {
+                            Log(string.Format("{0}: {1}", message.AuthorDetails.DisplayName, message.Snippet.DisplayMessage));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Login()
+        {
+            Properties.Settings.Default.clientId = txtClientId.Text;
+            Properties.Settings.Default.clientSecret = txtClientSecret.Text;
+            Properties.Settings.Default.Save();
+            Task.Run(async () =>
+            {
+                try
+                {
+                    Log("Initializing connection");
+
+                    connection = await YouTubeConnection.ConnectViaLocalhostOAuthBrowser(txtClientId.Text, txtClientSecret.Text, scopes);
+                    OnConnectionStateChanged();
+                }
+                catch (Exception ex)
+                {
+                    Log(ex.Message);
+                }
+            });
+        }
+
+        private delegate void SafeCallVoidDelegate();
+        private void OnConnectionStateChanged()
+        {
+            if (btnLogin.InvokeRequired)
+            {
+                btnLogin.Invoke(new SafeCallVoidDelegate(OnConnectionStateChanged));
+            }
+            else
+            {
+                btnLogin.Visible = false;
+                btnJoin.Visible = true;
+            }
+        }
+
+        private void Client_OnMessagesReceived(object sender, IEnumerable<LiveChatMessage> e)
+        {
+            foreach(var message in e)
+            {
+                Log(string.Format("{0}: {1}", message.AuthorDetails.DisplayName, message.Snippet.DisplayMessage));
+            }
+        }
+
+        private delegate void SafeCallDelegate(string text);
+        private void Log(string message)
+        {
+            if(txtLog.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(Log);
+                txtLog.Invoke(d, new object[] { message });
+            }
+            else
+            {
+                txtLog.Text += message + "\r\n";
+            }
+        }
+
+        private void txtChannel_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.channel = txtChannel.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            ConnectToChannel();
+        }
+
+        public void SendMessage(string message)
+        {
+            if (null == channel || null == connection) return;
+
+            new Task(async () =>
+            {
+                var broadcast = await connection.LiveBroadcasts.GetChannelActiveBroadcast(channel);
+
+                if (await connection.LiveBroadcasts.GetMyActiveBroadcast() != null)
+                {
+                    await client.SendMessage(message);
+                }
+            });
+        }
+    }
+}
